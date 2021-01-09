e2d3d49f40d21321a65e3a4128959f478663d568
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2021-01-10 02:18:29 +0700

    Initialize repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ui';
+  apiEndpoint = '';
+
+  constructor() {
+    this.apiEndpoint = environment.apiEndpoint;
+  }
 }
